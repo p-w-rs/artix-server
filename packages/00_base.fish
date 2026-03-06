856f41c7c758e60c33b8cfc924e8c00398770a65
Powers, Joshua <dev@powerslabs.org>
2026-03-06 11:43:53 -0700

    _

--- packages/00_base.fish
+++ packages/00_base.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
-basestrap /mnt base base-devel dinit fish parted
+basestrap /mnt base base-devel dinit fish parted dosfstools
 basestrap /mnt linux linux-firmware linux-headers linux-firmware linux-firmware-intel linux-firmware-nvidia
