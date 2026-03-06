788fc2f0589f9206af5576dd52e52494e703af0c
Powers, Joshua <dev@powerslabs.org>
2026-03-06 09:15:05 -0700

    _

--- packages/03_nvidia.fish
+++ packages/03_nvidia.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/env fish
 
-basestrap /mnt nvidia-open nvidia-open-dkms cuda cudnn nvtop
+basestrap /mnt nvidia-open nvidia-open-dkms nvidia-settings cuda cudnn nvtop gdb glu nsight-compute nsight-systems rdma-core opencl-nvidia
