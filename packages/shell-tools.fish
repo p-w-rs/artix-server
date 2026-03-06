3b2550681763cb3768cbe37a1a7f93db8b4d1ee6
Powers, Joshua <dev@powerslabs.org>
2026-03-06 13:57:10 -0700

    _

--- packages/shell-tools.fish
+++ packages/shell-tools.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
-set TOOLS    git less wget curl htop strace lsof btop eza bat nvme-cli parted dosfstools
+set TOOLS    helix git less wget curl htop strace lsof btop eza bat nvme-cli parted dosfstools
 set PACKAGES (string collect $PACKAGES $TOOLS)
