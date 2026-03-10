7f157623914ee73a0bafac0075054382921fe8a9
Powers, Joshua <joshua@powerslabs.org>
2026-03-10 12:01:37 -0600

    _

--- packages/services.sh
+++ packages/services.sh
@@ -1,4 +1,4 @@
 NET="connman connman-dinit wpa_supplicant wpa_supplicant-dinit openssh openssh-dinit nftables nftables-dinit"
-CORE="dbus dbus-dinit apparmor apparmor-dinit  cronie cronie-dinit"
+CORE="dbus dbus-dinit apparmor apparmor-dinit cronie cronie-dinit"
 OTHER="bluez bluez-dinit libnotify dunst"
 PACKAGES="$PACKAGES $NET $CORE $OTHER"
